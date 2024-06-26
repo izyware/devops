3234844a4d391e5aa3958fb44dbafa5feff3d416
christoff.dimitar <dimitarchristoff@izyware.com>
2024-06-26 18:32:54 +0000

    73000021: implement install script

---
+++ sh/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+{ # this ensures the entire script is downloaded #
+echo installing ...
+mkdir ~/izyware;
+cd ~/izyware;
+git clone git@github.com:izyware/devops.git;
+cd devops;
+npm install;
+npm link;
+echo izy.devops installed
+} # this ensures the entire script is downloaded #
