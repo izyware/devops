3be8e772123558e2573540b02100baa818c134ec
christoff.dimitar <dimitarchristoff@izyware.com>
2024-07-15 17:56:11 +0000

    73000025: update help message

--- help.js
+++ help.js
@@ -1,5 +1,8 @@
 /* izy-loadobject nodejs-require */
 module.exports = (function() {
-  const modtask = () => console.log(require('fs').readFileSync(`${__dirname}/README.md`).toString());
+  const modtask = () => {
+    console.log(require('fs').readFileSync(`${__dirname}/README.md`).toString());
+    console.log(`Installed at: ${__dirname}`);
+  };
   return modtask;
 })();
