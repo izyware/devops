5aef433b40a1e92dd4f53d84d4b2431028fd7013
christoff.dimitar <dimitarchristoff@izyware.com>
2024-09-04 19:26:29 +0000

    fix permissions

--- openvpn.js
+++ openvpn.js
@@ -4,7 +4,7 @@ module.exports = (function() {
   modtask.connect = async () => modtask.runSHScript('connect.sh');
 
   modtask.runSHScript = fileName => {
-    const path = `${__dirname}/rsync/${fileName}`;
+    const path = `${__dirname}/openvpn/${fileName}`;
     const args = process.argv.slice(4);
     require('child_process').spawn(path, args, { stdio: 'inherit' });
     return { success: true, data: '' };
